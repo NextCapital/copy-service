ba7fc60764727a34e75c147a741b3e79a90d987a
Mike Kreiser <kreiserm@nextcapital.com>
2025-05-12 12:27:11 -0500

    Add type definitions to SyntaxNode

--- js/copy-service/SyntaxNode/SyntaxNode.ts
+++ js/copy-service/SyntaxNode/SyntaxNode.ts
@@ -1,24 +1,21 @@
+import CopyService from '../CopyService';
+import IntlCopyService from '../IntlCopyService';
+
 /**
  * Base class for non-null AST nodes.
  */
 class SyntaxNode {
   /**
    * Returns `true` if the param is an AST node.
-   *
-   * @param {SyntaxNode|null} node
-   * @returns {boolean}
    */
-  static isAST(node) {
+  static isAST(node: SyntaxNode | null): boolean {
     return node === null || node instanceof SyntaxNode;
   }
 
   /**
    * If `null`, returns an empty string. Otherwise, calls `toSyntax` on the node.
-   *
-   * @param {SyntaxNode|null} node
-   * @returns {string}
    */
-  static safeToSyntax(node) {
+  static safeToSyntax(node: SyntaxNode | null): string {
     if (node === null) {
       return '';
     }
@@ -30,21 +27,16 @@ class SyntaxNode {
    * Converts the AST node to the syntax that made it.
    *
    * This method should be overridden in a subclass.
-   *
-   * @returns {string}
    */
-  toSyntax() {
+  toSyntax() : string {
     return '';
   }
 
   /**
    * Shortcut to the static `safeToSyntax` method.
-   *
-   * @param {SyntaxNode|null} node
-   * @returns {string}
    */
-  safeToSyntax(node) {
-    return this.constructor.safeToSyntax(node);
+  safeToSyntax(node: SyntaxNode | null) : string {
+    return (this.constructor as typeof SyntaxNode).safeToSyntax(node);
   }
 
   /**
@@ -53,13 +45,10 @@ class SyntaxNode {
    * when evaluated.
    *
    * This method should be overridden in a subclass.
-   *
-   * @param {CopyService|IntlCopyService} copyService
-   * @returns {boolean}
    */
-  isCacheable(copyService) { // eslint-disable-line no-unused-vars
+  isCacheable(copyService: CopyService | IntlCopyService) : boolean { // eslint-disable-line no-unused-vars
     return false;
   }
 }
 
-module.exports = SyntaxNode;
+export default SyntaxNode;

bf6657534a6f47857ed4ca45badacef05081e70c
Mike Kreiser <kreiserm@nextcapital.com>
2025-11-21 17:39:53 -0600

    Convert index.ts to use ESM import/export syntax [ENG-13456-ESM]

--- js/index.ts
+++ js/index.ts
@@ -1,43 +1,20 @@
-const Formatting = require('./copy-service/Formatting/Formatting').default;
-const Functional = require('./copy-service/Functional/Functional').default;
-const Newline = require('./copy-service/Newline/Newline').default;
-const Reference = require('./copy-service/Reference/Reference').default;
-const RefSubstitute = require('./copy-service/RefSubstitute/RefSubstitute').default;
-const Substitute = require('./copy-service/Substitute/Substitute').default;
-const Switch = require('./copy-service/Switch/Switch').default;
-const Verbatim = require('./copy-service/Verbatim/Verbatim').default;
-const WordBreak = require('./copy-service/WordBreak/WordBreak').default;
+export { default as Formatting } from './copy-service/Formatting/Formatting';
+export { default as Functional } from './copy-service/Functional/Functional';
+export { default as Newline } from './copy-service/Newline/Newline';
+export { default as Reference } from './copy-service/Reference/Reference';
+export { default as RefSubstitute } from './copy-service/RefSubstitute/RefSubstitute';
+export { default as Substitute } from './copy-service/Substitute/Substitute';
+export { default as Switch } from './copy-service/Switch/Switch';
+export { default as Verbatim } from './copy-service/Verbatim/Verbatim';
+export { default as WordBreak } from './copy-service/WordBreak/WordBreak';
 
-const Evaluator = require('./copy-service/Evaluator/Evaluator');
-const Substitutions = require('./copy-service/Substitutions/Substitutions').default;
+export { default as Evaluator } from './copy-service/Evaluator/Evaluator';
+export { default as Substitutions } from './copy-service/Substitutions/Substitutions';
 
-const ErrorHandler = require('./copy-service/ErrorHandler/ErrorHandler').default;
-const CopyService = require('./copy-service/CopyService').default;
-const IntlCopyService = require('./copy-service/IntlCopyService').default;
+export { default as ErrorHandler } from './copy-service/ErrorHandler/ErrorHandler';
+export { default as CopyService } from './copy-service/CopyService';
+export { default as IntlCopyService } from './copy-service/IntlCopyService';
 
-const PlainTextEvaluator = require('./plain-text-evaluator/PlainTextEvaluator').default;
-const HtmlEvaluator = require('./html-evaluator/HtmlEvaluator').default;
-const ReactEvaluator = require('./react-evaluator/ReactEvaluator').default;
-
-module.exports = {
-  Formatting,
-  Functional,
-  Newline,
-  Reference,
-  RefSubstitute,
-  Substitute,
-  Switch,
-  Verbatim,
-  WordBreak,
-
-  Evaluator,
-  Substitutions,
-
-  ErrorHandler,
-  CopyService,
-  IntlCopyService,
-
-  PlainTextEvaluator,
-  HtmlEvaluator,
-  ReactEvaluator
-};
+export { default as PlainTextEvaluator } from './plain-text-evaluator/PlainTextEvaluator';
+export { default as HtmlEvaluator } from './html-evaluator/HtmlEvaluator';
+export { default as ReactEvaluator } from './react-evaluator/ReactEvaluator';
